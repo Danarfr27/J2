05637c013c15e60cd8adb6ecf7f47d63e1d67c3b
Danarfr27 <123615449+Danarfr27@users.noreply.github.com>
2026-06-11 08:13:41 +0700

    Add SpecialBonusModal to App component

---
+++ src/components/ui/fraud.tsx
@@ -0,0 +1,17 @@
+// App.tsx
+
+import React from 'react';
+import SpecialBonusModal from './components/SpecialBonusModal'; // Sesuaikan path
+
+function App() {
+  return (
+    <div className="App">
+      {/* Konten aplikasi asli */}
+      <h1>Welcome to 7Meter</h1>
+      {/* ... */}
+      <SpecialBonusModal /> {/*[DAFTAR GRATIS] SPESIAL ULANG TAHUN 7METER SLOT HANYA DENGAN Rp50.000. BERPELUANG MENDAPATKAN 7X SUPPER SCATTER DIJAMIN MAXWIN !!!!*/}
+    </div>
+  );
+}
+
+export default App;
